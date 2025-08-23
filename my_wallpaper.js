a1d9de5bca3170fad01e2b57e5c48956708b9d9b
nui <monihungrykids@gmmail.com>
2025-08-23 15:58:18 +1200

    we back at it

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,28 +1,5 @@
 //your parameter variables go here!
-
- function nOtch (){
-  strokeWeight(1);
-    stroke (0, 0 , 0);
-    fill (247, 2, 6);
-    beginShape ();
-    vertex (50, 0);
-    vertex (54, 5);
-    vertex (54, 20);
-    vertex (50, 25);
-    endShape(CLOSE);
- }
-
-function bNotch (){
-  strokeWeight(1);
-  stroke (0, 0 , 0);
-  fill (247, 2, 6);
-  beginShape();
-  vertex (150, 0);
-  vertex (146, 5);
-  vertex (146, 20);
-  vertex (150, 25);
-  endShape(CLOSE);
-}
+var bGround = ("red")
 
 function setup_wallpaper(pWallpaper) {
  pWallpaper.output_mode(DEVELOP_GLYPH);
@@ -38,28 +15,16 @@ function setup_wallpaper(pWallpaper) {
 }
 
 function wallpaper_background() {
-  background(231, 242, 12); //yellow? more mimi than yellow, ew
+  background(bGround); //yellow? more mimi than yellow, ew
 }
 
 function my_symbol() {// do not rename this function. Treat this similarly to a Draw function
-
-  //grid lines
-  strokeWeight(1);
-  stroke(204, 39, 245)//light purple lines
-  line (0, 0, 200, 200);
-  line (0, 200, 200, 0);
-  line (0, 100, 200, 100);
-  line (100, 0, 100, 200);
-  line (50, 0, 50, 200);
-  line (0, 50, 200, 50);
-  line (150, 0, 150, 200);
-  line (0, 150, 200, 150);
-
+  
 for (let h = 0; h<=7; h++){
 push()
 translate( 0, 25*h)
-strokeWeight(1);
-stroke(255, 255, 255);
+strokeWeight(0.5);
+stroke(255, 2552, 255);
 fill (0, 0, 0);
 beginShape();
 vertex (0, 0);// the brain had to work overtime for this
@@ -80,7 +45,7 @@ push()
 translate( 50, 25*h)
 strokeWeight(1);
 stroke(255, 255, 255);
-fill (0, 0, 0);
+fill ("yellow");
 beginShape();
 vertex (0, 0);
 vertex (25, 25);
@@ -216,6 +181,14 @@ vertex (37.5, 12.5);
 vertex (25, 12.5);
 endShape(CLOSE);
 pop()}
+
+push()
+strokeWeight(1);
+stroke (252, 12, 8)//red
+line (23, 0, 23, 5);
+line (27, 0, 27, 5);
+
+pop();
 
 
 
