38b9228b6c68c09a6832fbc385538a200b04d9e3
nui <monihungrykids@gmmail.com>
2025-08-24 23:51:56 +1200

    25*a, 25*a goes diagonally?

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,27 +1,6 @@
 //your parameter variables go here!
-//all this here is actually dumb, realised my "variables" don't do jack
-//background colour
-var bGround = color = (247, 11, 7);//that was meant to be red, why not red?
-//stroke weight variables.//only thing that works
-let sW1 = (2);
-let sW2 = (2);
-let sW3 = (2);
-let sW4 = (3);
-//stroke colour variables//this isn't work, vhy?
-let sC1 = color = (0, 0, 0);//black
-let sC2 = color = (247, 11, 7);//red
-let sC3 = color = (2); 
-let sC4 = color = (3);
-//fill colour.//this doesn't work either, all show, no go
-let fil1 = color = (255, 255, 255);//white
-let fil2 = color = (255, 255, 255);
-let fil3 = color = (255, 255, 255);
-let fil4 = color =(255, 255, 255);
-//rotatiton variables.//it does something, so we'll take it
-let rOt1 = (5);//i dont quite know what this does but im going with it
-let rOt2 = (6);
-let rOt3 = (5);
-let rOt4 = (0);
+
+var cOl1 = (247, 147, 6);
 
 //list of colours to make it easier.
 // (0, 0, 0) white
@@ -33,23 +12,26 @@ let rOt4 = (0);
 // (7, 247, 11) green
 // (155, 7, 247) purple
 // (230, 225, 213) light grey
-//  orange
+// (247, 147, 6) orange
 
 //need a if statment.
 
-//test these column sets
+//wallpaper ideas
 //L = pasileafl, R = pasiLeafr.
 //L, L, R, R, L, L, R, R.
 //R, L, R, L, R, L, R, L.
 //R, R, L, L, R, R, L, L.
 //L, R, L, R, L, R, L, R.
-//go horizontal? rows
+//go horizontal/ rows
 //L, R, L, R, L, R, L, R.
 //R, L, R, L, R, L, R, L.
 //individual sets
-//L, R, L, R, L, R, L, R.
+//L, R, L, R, L, R, L, R.//change every row/column.
 //R, L, R, L, R, L, R, L.
-//
+//change colors?
+//bGround.
+//fills.
+//stroke.
 
 //pasi leaf pattern shape, use translate jitsu to call this.
 //pasileaf L and R are made at 0, 0, so translating should be easier.
@@ -101,10 +83,19 @@ function setup_wallpaper(pWallpaper) {
 }
 
 function wallpaper_background() {
-  background(247, 147, 6); //background colour
+  background(0, 0, 0); //background colour
 }
 
 function my_symbol() {// do not rename this function. Treat this similarly to a Draw function
 
-
+//
+for (let a = 0; a <= 7; a++){
+  push ();
+  translate (25*a, 25*a);
+  strokeWeight(1);
+  stroke (247, 147, 6);
+  fill (247, 11, 7);
+  pasiLeafr();
+  pop();
+}
 }
